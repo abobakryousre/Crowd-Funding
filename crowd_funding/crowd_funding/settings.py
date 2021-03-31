f87f819a02e083c980e25a14fd21c1af25465074
Abobakr <abobakryousre@gmail.com>
2021-03-31 09:08:21 +0200

    feat(database-schema): configure mysql wiht databaase name 'crowd_funding'

--- crowd_funding/crowd_funding/settings.py
+++ crowd_funding/crowd_funding/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'crowd_funding.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crowd_funding',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
