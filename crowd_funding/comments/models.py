d5c6e2abfdc37a938f9708ee71a08ef43645d431
Abobakr <abobakryousre@gmail.com>
2021-03-31 13:40:52 +0200

    feat(database-schema): update the __str__ function, and report_count in ReprotedComment model

--- crowd_funding/comments/models.py
+++ crowd_funding/comments/models.py
@@ -6,8 +6,11 @@ from django.db import models
  you should first to have a commint to report it,
  to check if the comment is reported or not use hasattr method -> hasattr(the objectInstance, 'reported_comment')
 
- to get reportedComment -> objectInstance.reported_comment
-
+ to get ReportedComment object -> objectInstance.reportedcomment
+ to get the report count  -> objectInstance.reportedcomment.report_count
+ ************************************************************************
+ # to create a reprot for some comment 
+ report = ReportedComment.objects.create(comment=CommentobjectInstance)
 
 """
 
@@ -21,9 +24,9 @@ class Comments(models.Model):
 
 class ReportedComment(models.Model):
     # the comment has only one reportedComment object
-    report_count = models.IntegerField()
+    report_count = models.IntegerField(default=1)
     comment = models.OneToOneField(
         Comments, on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self):
-        return self.report_count
+        return str(self.report_count)
