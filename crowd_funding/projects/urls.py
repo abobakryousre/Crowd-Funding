77f928710f7d585be52e007aa120829a55ad68e1
Abobakr <abobakryousre@gmail.com>
2021-04-02 20:40:20 +0200

    feat(home-page-layout): create base template with  navebar and footer

--- crowd_funding/projects/urls.py
+++ crowd_funding/projects/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-
+from .views import index
 urlpatterns = [
-    path('', ),
+    path('', index, name="projects" ),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
