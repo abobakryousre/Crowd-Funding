2976b944fadf0c19005286b8bf2db71eea8a1b5a
Abobakr <abobakryousre@gmail.com>
2021-04-03 22:26:55 +0200

    feat(user-profile): delete account using ajax request

--- crowd_funding/users/ursl.py
+++ crowd_funding/users/ursl.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import index, edit_profile, view_profile
+from .views import index, edit_profile, view_profile, check_password, delete_account
 urlpatterns = [
     path('', index, name="index"),
     path("profile/edit", edit_profile, name="edit-profile"),
     path("profile/", view_profile, name="profile"),
+    path('profile/check-password', check_password, name="check-password"),
+    path('delete-account/', delete_account, name='delete-account'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- crowd_funding/users/views.py
+++ crowd_funding/users/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.shortcuts import get_object_or_404
 from .models import User
 from .forms import UserProfile
+from django.http import JsonResponse
+
+
 # Create your views here.
 def index(request):
     return render(request, 'users/index.html')
+
 
 def edit_profile(request):
     if request.method == "GET":
@@ -26,8 +30,23 @@ def edit_profile(request):
             context = {"user_profile": updated_data}
             return render(request, "users/edit_profile.html", context)
 
+
 def view_profile(request):
     # get user information
-    current_user = User.objects.first() # will change with request.usr.pk
+    current_user = User.objects.first()  # will change with request.usr.pk
     context = {"current_user": current_user}
-    return render(request, 'users/profile.html', context )
+    return render(request, 'users/profile.html', context)
+
+
+def check_password(request):
+    if request.is_ajax():
+        password = request.GET.get("password");
+        user = User.objects.first()
+        if user.password == password:
+            return JsonResponse({"isPasswordCorrect": True})
+        else:
+            return JsonResponse({"isPasswordCorrect": False})
+    else:
+        return HttpResponse("Page Not Found!");
+def delete_account(request):
+    return render(request, 'users/index.html')
