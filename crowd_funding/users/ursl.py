fe95f55b0a80df7bf7d965a5d578156372194d1a
OmniaMostafa98 <omniamostafa1998@gmail.com>
2021-04-04 20:13:28 +0200

    feat(user-profile-show-projects): Adding show projects and show donations and delete project

--- crowd_funding/users/ursl.py
+++ crowd_funding/users/ursl.py
@@ -1,8 +1,12 @@
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', ),
+    url(r'^profile$',views.index ),
+    url(r'^deleteItem/(?P<pk>\d+)$', views.deleteItem,name='deleteItem'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- crowd_funding/users/views.py
+++ crowd_funding/users/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+
+from django.http import HttpResponse
+from projects.models import Projects, Images
+from projects.models.projects import Donation
+from django.db.models import Sum
+
+
+def index(request):
+
+    projects_of_specific_user = Projects.objects.filter(user_id = 1)
+    donations=Donation.objects.select_related('project').filter(user_id=1)
+
+
+    return render(request, 'users/profile.html',{"projects":projects_of_specific_user,"donations":donations})
+
+
+
+def deleteItem(request,pk):
+    sum_donations_of_project = Donation.objects.values('project_id').order_by('project_id').annotate(total_price=Sum('amount')).get(project_id=pk)
+
+    project = Projects.objects.get(id=pk)
+
+   # return HttpResponse(str(project.total_target))
+    if sum_donations_of_project["total_price"] >= project.total_target*(25/100):
+
+        return HttpResponse("Sorry,You can't delete this project because its donations exceeds 25% of total target")
+
+    else:
+        project.delete()
+        return HttpResponse("Done")
+
+
+
+
+
+
+
+
+
+
+
+
+
