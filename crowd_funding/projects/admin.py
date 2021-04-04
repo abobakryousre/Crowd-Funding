0c4f27bdc5adde53158b26ee3c23889cc3d98985
Abobakr <abobakryousre@gmail.com>
2021-04-04 19:10:14 +0200

    feat(home-page-layout): make the silder and latest project dynamic

--- crowd_funding/projects/admin.py
+++ crowd_funding/projects/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from projects.models import Projects
-from comments.models import Comments
+from projects.models import Projects, Rating, Tags, Images, ReportedProject
+from comments.models import Comments, ReportedComment
+
 # Register your models here.
 
 admin.site.register(Projects)
+admin.site.register(Rating)
+admin.site.register(Tags)
+admin.site.register(Images)
+admin.site.register(ReportedProject)
 admin.site.register(Comments)
+admin.site.register(ReportedComment)
--- crowd_funding/users/views.py
+++ crowd_funding/users/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+from projects.models import Projects, Rating
 # Create your views here.
 def index(request):
-    return render(request, 'users/index.html')
+    # TODO: slider, for highest 5 reated projects
+    # get the highest 5 rate
+    highest_rate = Rating.objects.all().order_by('-five_star')[:5]
+    highest_rated_projects = []
+    for rate in highest_rate:
+        highest_rated_projects.append(rate.project)
+    # TODO: five-latest-projects
+    latest_five_projects = Projects.objects.all().order_by('-created_at')[:5]
+    # TODO: list of 5 projects selected by admin
+    # TODO: category and it's projects
+    # TODO: search bar, with projects tag, and title.
+    context = {
+        'highest_projects': highest_rated_projects,
+        'latest_projects': latest_five_projects
+    }
+    return render(request, 'users/index.html', context)
