4cbd25d53d5093aa2779bfc297541732b0215bba
SarahOuf <sarah.m.ouf@gmail.com>
2021-04-05 01:53:10 +0200

    feat(view-project): modified project_page style

--- crowd_funding/projects/admin.py
+++ crowd_funding/projects/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from projects.models import Projects
 from comments.models import Comments
 from projects.models.projects import Category
+from projects.models import Images
 # Register your models here.
 
 admin.site.register(Projects)
 admin.site.register(Comments)
 admin.site.register(Category)
+admin.site.register(Images)
