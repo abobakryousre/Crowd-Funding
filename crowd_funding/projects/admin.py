aaf47e0f3d78752fd7ea95f6fddf120c0dde0a8a
SarahOuf <sarah.m.ouf@gmail.com>
2021-04-05 00:37:59 +0200

    feat(view-project): added Category model to admin site

--- crowd_funding/projects/admin.py
+++ crowd_funding/projects/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from projects.models import Projects
 from comments.models import Comments
+from projects.models.projects import Category
 # Register your models here.
 
 admin.site.register(Projects)
 admin.site.register(Comments)
+admin.site.register(Category)
