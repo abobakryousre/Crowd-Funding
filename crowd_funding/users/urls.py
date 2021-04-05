69feb472d6d0044aa5a43f4b0e8629d5caac2769
Abobakr <abobakryousre@gmail.com>
2021-04-06 01:19:17 +0200

    feat(home-page-layout): activate search bar

--- crowd_funding/users/urls.py
+++ crowd_funding/users/urls.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from users.views import index, display_category
+from users.views import index, display_category, search_for_projects
 urlpatterns = [
     path('', index),
     path('display-category/', display_category, name='display-category'),
+    path('search-for-projects', search_for_projects, name='search-for-projects'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
