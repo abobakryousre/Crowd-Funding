15e496602f99b157b777006b0955d3e80a875c5a
GhadaRagab123 <ghadar109@gmail.com>
2021-04-06 21:12:07 +0200

    feat(register-user):authrantcation without email activation

--- crowd_funding/users/views.py
+++ crowd_funding/users/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
-
+#fun of registration
 def UserRegisterView(request):
 
     form = SignUpForm()
@@ -26,7 +26,7 @@ def UserRegisterView(request):
     context = {'form': form}
     return render(request, 'users/form.html', context)
 
-
+# log in fun
 def loginPage(request):
 
     if request.method == 'POST':
@@ -44,11 +44,11 @@ def loginPage(request):
     context = {}
     return render(request, 'users/login.html', context)
 
-
+#logout fun will used in bakr home page
 def logoutUser(request):
     logout(request)
     return redirect('login')
-
+# tried fun using temprary in home page to can render home url
 def index(request):
     return HttpResponse("Hello world")
 
@@ -56,74 +56,3 @@ def index(request):
 
 
 
-# def registerpage(request):
-#     # render form
-#     if request.method == 'GET':
-#         form = createuserform()
-#         context = {
-#             'formregister': form
-#         }
-#         return render(request, 'users/form.html', context)
-#
-#         # check on data
-#         # is valied => save data on database
-#         # not => render form again
-#     else:
-#         fulldata = createuserform(request.POST, request.FILES)
-#
-#         if fulldata.is_valid():
-#             fulldata.save()
-#             # email=fulldata.cleaned_data('email')
-#             # raw_password=fulldata.cleaned_data('password1')
-#             # users=authenticate(email=email,passwor=raw_password)
-#             # login(request,users)
-#             return render(request,'users/home.html')
-#         else:
-#             context = {"formregister": fulldata}
-#             return render(request, 'users/form.html', context)
-#
-#
-# def loginform(request):
-#     if request.method == 'POST':
-#         email = request.POST.get('email')
-#         password = request.POST.get('password')
-#
-#         user = authenticate(request, email=email, password=password)
-#
-#         if user is not None:
-#             login(request, user)
-#             return render('users/home.html')
-#         else:
-#             messages.info(request, 'Email OR password is incorrect')
-#
-#     context = {}
-#     return render(request, 'users/login.html', context)
-
-    ########################################
-    # context = {}
-    # if request.method == 'POST':
-    #     print('loginnnnnnnnnn')
-    #     form = loginformauth()
-    #     context = {
-    #         'formlogin': form
-    #     }
-    #     return render(request, 'users/login.html', context)
-    # else:
-    #     user = request.user
-    #     if user.is_authenticated:
-    #         print('uuuuuuseerr utttttttttttttttt')
-    #
-    #         return render(request, 'users/home.html')
-    #     if request.POST:
-    #         form = loginformauth(request.POST)
-    #         if form.is_valid():
-    #             email = request.POST['email']
-    #             password = request.POST['password']
-    #             user = authenticate(email=email, passwor=password)
-    #             if user:
-    #                 print('uuuuuuseerr')
-    #                 login(request, user)
-    #                 return render(request, 'users/home.html')
-    #
-    #
-    #
