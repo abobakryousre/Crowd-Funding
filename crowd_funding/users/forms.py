e0214f8f9b2c936ca6896321c784f0ef209d5a90
Abobakr <abobakryousre@gmail.com>
2021-04-06 21:22:18 +0200

    feat(user-profile): styling edit profile form

--- crowd_funding/users/forms.py
+++ crowd_funding/users/forms.py
@@ -1,8 +1,17 @@
-from django.forms import ModelForm
+from django import forms
 from users.models import User
 
-class UserProfile(ModelForm):
+class UserProfile(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'country', 'profile_picutre', 'mobile_phone', 'birth_date']
 
+    widgets = {
+        "first_name": forms.TextInput(attrs={ 'class': 'form-control' }),
+        "last_name": forms.TextInput(attrs={ 'class': 'form-control' }),
+        "country": forms.Select(attrs={ 'class': 'form-control' }),
+        "profile_picutre": forms.FileInput(attrs={ 'class': 'form-control' }),
+        "mobile_phone": forms.NumberInput(attrs={ 'class': 'form-control' }),
+        "birth_date": forms.DateInput(attrs={ 'class': 'form-control' }),
+
+    }
