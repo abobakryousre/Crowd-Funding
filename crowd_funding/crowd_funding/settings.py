2fb46e4b0d79ac9682579e8012ffe12737e42f9a
GhadaRagab123 <ghadar109@gmail.com>
2021-04-08 17:36:07 +0200

    feat(feature-email-active):make email active with spacefic google email

--- crowd_funding/crowd_funding/settings.py
+++ crowd_funding/crowd_funding/settings.py
@@ -149,7 +149,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'crowedfunding@gmail.com'
-# EMAIL_HOST_PASSWORD = 'crowed109'
-EMAIL_HOST_USER ='ghadar109@gmail.com'
-EMAIL_HOST_PASSWORD = '01146358766'
+EMAIL_HOST_USER = 'crowedfunding@gmail.com'
+EMAIL_HOST_PASSWORD = 'crowed109'
+# EMAIL_HOST_USER ='ghadar109@gmail.com'
+
+# EMAIL_HOST_PASSWORD = '01146358766'
